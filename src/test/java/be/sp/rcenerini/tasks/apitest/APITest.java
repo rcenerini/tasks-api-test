33a4bc22a01c2775766530d95103993037e16582
estagiariodecloud <estagiariode.cloud@gmail.com>
2020-11-05 15:33:53 -0300

    vai

--- src/test/java/be/sp/rcenerini/tasks/apitest/APITest.java
+++ src/test/java/be/sp/rcenerini/tasks/apitest/APITest.java
@@ -1,5 +1,4 @@
 package be.sp.rcenerini.tasks.apitest;
-
 import org.hamcrest.CoreMatchers;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -13,65 +12,58 @@ public class APITest {
 	public static void setup() {
 		RestAssured.baseURI = "http://100.25.148.10:8080/tasks-backend";
 	}
-	
+
 	@Test
 	public void deveRetornarTarefas() {
 		RestAssured.given()
 		.when()
 			.get("/todo")
 		.then()
-			.statusCode(200)			
+			.statusCode(200)
 		;
-		
 	}
 	
 	@Test
 	public void deveAdicionarTarefaComSucesso() {
 		RestAssured.given()
-			.body("{\r\n"
-					+ "	\"task\": \"Teste via API\", \"dueDate\": \"2020-12-30\"\r\n"
-					+ "}").contentType(ContentType.JSON)		
+			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2020-12-30\" }")
+			.contentType(ContentType.JSON)
 		.when()
 			.post("/todo")
 		.then()
 			.statusCode(201)
-		
 		;
 	}
-
+	
 	@Test
-	public void naodeveAdicionarTarefaInvalida() {
+	public void naoDeveAdicionarTarefaInvalida() {
 		RestAssured.given()
-			.body("{\r\n"
-					+ "	\"task\": \"Teste via API\", \"dueDate\": \"2020-12-30\"\r\n"
-					+ "}").contentType(ContentType.JSON)		
+			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2010-12-30\" }")
+			.contentType(ContentType.JSON)
 		.when()
 			.post("/todo")
 		.then()
-			.log().all()
 			.statusCode(400)
 			.body("message", CoreMatchers.is("Due date must not be in past"))
-		
 		;
 	}
 	
 	@Test
 	public void deveRemoverTarefaComSucesso() {
-		//inserir
+
 		Integer id = RestAssured.given()
 			.body("{ \"task\": \"Tarefa para remoção\", \"dueDate\": \"2020-12-30\" }")
 			.contentType(ContentType.JSON)
 		.when()
 			.post("/todo")
 		.then()
-//			.log().all()
+
 			.statusCode(201)
 			.extract().path("id")
 		;
 		
 		System.out.println(id);
 		
-		;
 	}
 }
 
